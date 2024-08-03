1e7433c7f4ddd2eb29422db45a234c4578e117c7
Jungsun <ijs_1016@kookmin.ac.kr>
2024-08-03 17:06:48 +0900

    Title: 겹치지 않게 선분 고르기, Time: 167ms, Memory: 26MB, Status: Passed - CodeTree

--- 240803/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240803/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -4,13 +4,12 @@ lines = [list(map(int, input().split())) for _ in range(N)]
 def is_overlap(i_line, n_lines) :
     if len(n_lines) == 0 :
         return False
-
     ix1, ix2 = i_line
     for n_line in n_lines :
         x1, x2 = n_line
-        if ((ix1 < x1 and ix2 < x1) or (x1 < ix1 and x2 < ix1)) :
-            return False
-    return True
+        if not ((ix1 < x1 and ix2 < x1) or (x1 < ix1 and x2 < ix1)) :
+            return True
+    return False
 
 result = 0
 def find_max_line_wo_overlap(i, n_lines) :
